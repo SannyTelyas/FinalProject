bb3f3e0b89535826bd0ac25520b3d4b2cab58f6d
sannyTelyas <sanny4454@gmail.com>
2022-07-04 10:58:51 +0300

    add

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./CSS/App.css"
+import 'mdb-react-ui-kit/dist/css/mdb.min.css'
+import Header from "./Components/Header+Footer/Header"
+import Footer from "./Components/Header+Footer/Footer"
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      תוכן אתר 
+      
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;

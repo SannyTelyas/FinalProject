0de70d9067070ec8ffdcbd64954734185ceda935
sannyTelyas <sanny4454@gmail.com>
2022-07-20 11:22:52 +0300

    Add new header to github

--- src/Components/Header+Footer/Header.jsx
+++ src/Components/Header+Footer/Header.jsx
@@ -43,13 +43,7 @@ export default function Header() {
         </MDBContainer>
       </MDBNavbar>
 
-      <div className='p-5 text-center bg-light'>
-        <h1 className='mb-3'>Heading</h1>
-        <h4 className='mb-3'>Subheading</h4>
-        <a className='btn btn-primary' href='' role='button'>
-          Call to action
-        </a>
-      </div>
+  
     </header>
   );
 }

f8d744982542880dc127fd91989afaf9c1fa5ffb
sannyTelyas <sanny4454@gmail.com>
2022-07-20 11:02:05 +0300

    About Up file sanny

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import "../json/jsonFile.json";
 
 function thisPage1() {
-    const testJson = require("../json/jsonFile.json")
+    const testJson = require("../json/jsonFile.json"); 
+    console.log(testJson[0].AboutManager); 
     return(
         <div>
             
             <div>על המועדון: {testJson[0].About}</div>
-            <div>על מנהל המועדון: {testJson[0].AboutManager}</div>
+            <div>על מנהל המועדון: {testJson[0].AboutManger}</div>
 
         </div>
         
